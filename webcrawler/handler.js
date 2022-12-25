8c247ffdfb2df2259d4ccf4e75d5c826bbdf2fc3
Ting <chienweiting3839@gmail.com>
2022-12-26 05:43:40 +0800

    Add parsing of bestseller list titles

--- webcrawler/handler.js
+++ webcrawler/handler.js
@@ -1,6 +1,7 @@
 'use strict';
 
 const axios = require('axios');
+const cheerio = require('cheerio');
 
 function getBestsellerList() {
   return axios.get(
@@ -9,9 +10,19 @@ function getBestsellerList() {
 }
 
 module.exports.scrape = async (event, context, callback) => {
-  Promise.all([getBestsellerList()]).then(data => {
-    console.log(data);
-    callback(null, data[0]);
+  Promise.all([getBestsellerList()]).then(res => {
+    //console.log(res[0].data);
+    const $ = cheerio.load(res[0].data);
+    //console.log($);
+    const bookList = $('.list-wrapper ul');
+    for (let i = 0; i < bookList.length; i++) {
+      const bookLi = bookList.eq(i).find('li');
+      const title = bookLi.find('.title a').text();
+      console.log(title);
+    }
+    console.log(bookList);
+    //console.log(bookList);
+    callback(null, bookList);
   });
 
   // Use this code if you don't use the http event with the LAMBDA-PROXY integration
